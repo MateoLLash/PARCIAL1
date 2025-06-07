3876d4d24d88dfcdfd23e5c20d09fa8c229fab66
MateoLL <mllashag2006@gmail.com>
2025-06-07 00:59:21 -0500

    Primer proyeto

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def consultar_cartera():
 if __name__ == '__main__':
     if not os.path.exists(app.config ['UPLOAD_FOLDER']):
         os.makedirs(app.config [ 'UPLOAD_FOLDER'])
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))pip freeze >requirements.txt
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
